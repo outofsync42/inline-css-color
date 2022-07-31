37c5fd37c13468481a3bcafcdb084cbd9404e140
kevinkhantzis <kevinkhantzis@outlook.com>
2022-07-31 12:06:58 -0700

    improved caching

--- extension.js
+++ extension.js
@@ -1,3 +1,4 @@
+const { start } = require('repl');
 const vscode = require('vscode');
 const Application = require('./lib/application');
 
@@ -39,6 +40,125 @@ var InlineCssColor = function (application) {
 		string: {},
 	}
 
+	this.setLineRanges = function (lines, i) {
+
+		let line_id = lines[i]['id'];
+
+		for (var type in ranges) {
+			ranges[type][line_id] = [];
+		}
+
+		let elements = lines[i]['html']['elements'];
+
+		for (var element_id in elements) {
+			let style = elements[element_id]['attributes']['style'];
+			if (style) {
+				for (var index in style) {
+					let line = style[index]['line'];
+					let text = lines[line]['text'];
+					let start = style[index]['start'];
+					let end = style[index]['end'];
+					//console.log(lines[line]);
+					if (start === null || end === null) {
+						continue;
+					}
+
+					let x = start;
+					let numericStart = null;
+					let constStart = null;
+					let stringStart = null;
+					let phpOpen = false;
+					let inMatch = false;
+
+					while (x <= end) {
+
+						if (text.indexOf(/<\?/, x) === x) {
+							phpOpen = true;
+						} else if (text.indexOf(/\?>/, x) === x) {
+							phpOpen = false;
+						}
+
+						if (phpOpen == false) {
+							let valueEnd = false;
+							let supportFunction = false;
+							if (text.indexOf(/:| :|  :/, x) == x) {
+								if (constStart !== null) {
+									ranges['keywords'][line_id].push(new vscode.Range(line, constStart, line, x));
+									inMatch = false;
+									constStart = null;
+								}
+								ranges['punctuation'][line_id].push(new vscode.Range(line, x, line, x + 1));
+							} else if (text.indexOf(/[),=]/, x) == x) {
+								ranges['punctuation'][line_id].push(new vscode.Range(line, x, line, x + 1));
+								valueEnd = true;
+							} else if (text.indexOf(/[\(]/, x) == x) {
+								ranges['punctuation'][line_id].push(new vscode.Range(line, x, line, x + 1));
+								valueEnd = true;
+								supportFunction = true;
+							} else if (text.indexOf(/[;]/, x) == x) {
+								ranges['punctuation'][line_id].push(new vscode.Range(line, x, line, x + 1));
+								valueEnd = true;
+							} else if (text.indexOf(/[ ]/, x) == x) {
+								valueEnd = true;
+							} else if (x == end) {
+								valueEnd = true;
+							} else if (inMatch == false && text.indexOf(/['"]/, x) == x) {
+								if (stringStart === null) {
+									stringStart = x;
+									inMatch = true;
+								}
+							} else if (inMatch == false && text.indexOf(/[0-9#]/, x) == x) {
+								if (numericStart === null) {
+									numericStart = x;
+									inMatch = true;
+								}
+							} else if (inMatch == false && text.indexOf(/[a-zA-Z]/, x) == x) {
+								if (constStart === null) {
+									constStart = x;
+									inMatch = true
+								}
+							} else if (inMatch == false && text.indexOf(/[-]/, x) == x && text.indexOf(/[0-9]/, x + 1) == x + 1) {
+								if (numericStart === null) {
+									numericStart = x;
+									inMatch = true;
+								}
+							} else if (inMatch == false && text.indexOf(/[-]/, x) == x && text.indexOf(/[a-zA-Z]/, x + 1) == x + 1) {
+								if (constStart === null) {
+									constStart = x;
+									inMatch = true;
+								}
+							}
+
+							if (valueEnd) {
+								inMatch = false;
+								if (stringStart) {
+									ranges['string'][line_id].push(new vscode.Range(line, stringStart, line, x));
+									stringStart = null;
+								}
+								if (numericStart !== null) {
+									ranges['valueNumeric'][line_id].push(new vscode.Range(line, numericStart, line, x + (x == end && isset(text[x + 1]) == false ? 1 : 0)));
+									numericStart = null;
+								}
+								if (constStart !== null) {
+									if (supportFunction) {
+										supportFunction = false;
+										ranges['supportFunction'][line_id].push(new vscode.Range(line, constStart, line, x + (x == end && isset(text[x + 1]) == false ? 1 : 0)));
+									} else {
+										ranges['valueConstant'][line_id].push(new vscode.Range(line, constStart, line, x + (x == end && isset(text[x + 1]) == false ? 1 : 0)));
+									}
+									constStart = null;
+								}
+							}
+						}
+						x++;
+					}
+
+				}
+
+			}
+		}
+	}
+
 	this.colorLines = function (startLine, endLine, diff) {
 
 		let editor = application.editor();
@@ -48,144 +168,38 @@ var InlineCssColor = function (application) {
 		startLine = is_int(startLine) ? startLine : 0
 		endLine = is_int(endLine) ? endLine : lines.length - 1; //inclusive
 
-		let validIds = {}; //more performant to index rather than use array
-		for (var i in lines) {
-			let line_id = lines[i]['id'];
-			validIds[line_id] = 0;
-			if (lines[i]['syntax'] == 'html') {
-				if ((i >= startLine && i <= endLine)) {
 
-					for (var type in ranges) {
-						ranges[type][line_id] = [];
-					}
 
-					let elements = lines[i]['html']['elements'];
-					for (var o in elements) {
-						let style = elements[o]['attributes']['style'];
-						if (style) {
+		let validIds = {};
+		let isSingleLine = diff == 0 && startLine == endLine;
 
-							let line = style['line'];
-							let text = lines[line]['text'];
-							let startPos = style['valueStartPos'];
-							let endPos = style['valueEndPos'];
-							if (startPos === null || endPos === null) {
-								continue;
-							}
-
-							let x = startPos;
-							let numericStart = null;
-							let constStart = null;
-							let stringStart = null;
-							let phpOpen = false;
-							let inMatch = false;
-							let openQuote = '';
-							while (openQuote == '' && x > -1) {
-								x--;
-								if (text[x] == '"' || text[x] == "'") {
-									openQuote = text[x];
-								}
-							}
-							if (x == -1) {
-								console.log('error finding opening quote on line ' + line);
-								continue;
-							}
-							x++;
-							let openString = openQuote == "'" ? '"' : "'";
-
-							while (x <= endPos && x < text.length) {
-
-								if (text.indexOf(/<\?/, x) === x) {
-									phpOpen = true;
-								} else if (text.indexOf(/\?>/, x) === x) {
-									phpOpen = false;
-								}
-
-								if (phpOpen == false) {
-									let valueEnd = false;
-									let supportFunction = false;
-									if (text.indexOf(/:| :|  :/, x) == x) {
-										if (constStart) {
-											ranges['keywords'][line_id].push(new vscode.Range(line, constStart, line, x));
-											inMatch = false;
-											constStart = null;
-										}
-										ranges['punctuation'][line_id].push(new vscode.Range(line, x, line, x + 1));
-									} else if (text.indexOf(/[),=]/, x) == x) {
-										ranges['punctuation'][line_id].push(new vscode.Range(line, x, line, x + 1));
-										valueEnd = true;
-									} else if (text.indexOf(/[\(]/, x) == x) {
-										ranges['punctuation'][line_id].push(new vscode.Range(line, x, line, x + 1));
-										valueEnd = true;
-										supportFunction = true;
-									} else if (text.indexOf(/[;]/, x) == x) {
-
-										ranges['punctuation'][line_id].push(new vscode.Range(line, x, line, x + 1));
-
-										valueEnd = true;
-									} else if (text.indexOf(/[ ]/, x) == x) {
-										valueEnd = true;
-									} else if (text.indexOf(openQuote, x) == x) {
-										valueEnd = true;
-									} else if (inMatch == false && text.indexOf(openString, x) == x) {
-										if (stringStart === null) {
-											stringStart = x;
-											inMatch = true;
-										}
-									} else if (inMatch == false && text.indexOf(/[0-9#]/, x) == x) {
-										if (numericStart === null) {
-											numericStart = x;
-											inMatch = true;
-										}
-									} else if (inMatch == false && text.indexOf(/[a-zA-Z]/, x) == x) {
-										if (constStart === null) {
-											constStart = x;
-											inMatch = true
-										}
-									} else if (inMatch == false && text.indexOf(/[-]/, x) == x && text.indexOf(/[0-9]/, x + 1) == x + 1) {
-										if (numericStart === null) {
-											numericStart = x;
-											inMatch = true;
-										}
-									} else if (inMatch == false && text.indexOf(/[-]/, x) == x && text.indexOf(/[a-zA-Z]/, x + 1) == x + 1) {
-										if (constStart === null) {
-											constStart = x;
-											inMatch = true;
-										}
-									}
-
-									if (valueEnd) {
-										inMatch = false;
-										if (stringStart) {
-											ranges['string'][line_id].push(new vscode.Range(line, stringStart, line, x));
-											stringStart = null;
-										}
-										if (numericStart) {
-											ranges['valueNumeric'][line_id].push(new vscode.Range(line, numericStart, line, x));
-											numericStart = null;
-										}
-										if (constStart) {
-											if (supportFunction) {
-												supportFunction = false;
-												ranges['supportFunction'][line_id].push(new vscode.Range(line, constStart, line, x));
-											} else {
-												ranges['valueConstant'][line_id].push(new vscode.Range(line, constStart, line, x));
-											}
-											constStart = null;
-										}
-									}
-								}
-								x++;
-							}
-						}
+		if (isSingleLine) {
+			if (lines[startLine]['syntax'] == 'html') {
+				if (isset(lines[startLine]['line_links'])) {
+					for (var i in lines[startLine]['line_links']) {
+						self.setLineRanges(lines, lines[startLine]['line_links'][i]);
 					}
 				}
+				self.setLineRanges(lines, startLine);
 			}
-			if (i > endLine) {
-				if (diff !== 0) {
-					for (var type in ranges) {
-						for (var o in ranges[type][line_id]) {
-							ranges[type][line_id][o]['_start']['_line'] += diff;
-							ranges[type][line_id][o]['_end']['_line'] += diff;
+		} else {
+
+			for (var i in lines) {
+				i = parseInt(i)
+				let line_id = lines[i]['id'];
+				validIds[line_id] = 0;
+				if (lines[i]['syntax'] == 'html') {
+					if ((i >= startLine && i <= endLine)) {
+						self.setLineRanges(lines, i);
+					}
+				}
+				if (i > endLine) {
+					if (diff !== 0) {
+						for (var type in ranges) {
+							for (var o in ranges[type][line_id]) {
+								ranges[type][line_id][o]['_start']['_line'] += diff;
+								ranges[type][line_id][o]['_end']['_line'] += diff;
+							}
 						}
 					}
 				}
@@ -197,7 +211,7 @@ var InlineCssColor = function (application) {
 		for (var type in ranges) {
 			decorationRanges[type] = [];
 			for (var line_id in ranges[type]) {
-				if (isset(validIds[line_id]) == false) {
+				if (isSingleLine == false && isset(validIds[line_id]) == false) {
 					delete ranges[type][line_id];
 				} else {
 					for (let i in ranges[type][line_id]) {
@@ -240,7 +254,7 @@ function activate(context) {
 			console.log(e);
 		}
 	})
-	application.on('documentTextChange', function (e, startLine, endLine, diff) {
+	application.on('documentTextChange', function (startLine, endLine, diff) {
 		try {
 			controls[application.documentPath()].colorLines(startLine, endLine, diff);
 		} catch (e) {
@@ -257,3 +271,4 @@ module.exports = {
 	activate,
 	deactivate
 }
+
